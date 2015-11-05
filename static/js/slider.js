d25d602a2a092115df981e35a8a33eb3f892719d
David Rivas <d.rivas95@hotmail.com>
2015-11-05 09:22:44 -0500

    Slider final

--- static/js/slider.js
+++ static/js/slider.js
@@ -1,35 +1,38 @@
 (function() {
     var windowWidth = window.innerWidth,
         $sliderImages = document.getElementsByClassName('sliderImages')[0],
-        sliderButtons = Array.prototype.slice.call(document.getElementsByClassName('sliderButton')),
+        sliderButtons = document.getElementsByClassName('sliderButton'),
         cantButtons = sliderButtons.length,
-        $imageSlider = document.getElementsByClassName('sliderList');
+        $sliderList = document.getElementsByClassName('sliderList');
         
     function sliderWidth() {
         var howManyChildren = $sliderImages.children.length;
             
-        for (i = 0; i < $imageSlider.length; i++) {
-            $imageSlider[i].style.width = windowWidth + "px"; 
+        for (i = 0; i < $sliderList.length; i++) {
+            $sliderList[i].style.width = windowWidth + "px"; 
         }
 
         $sliderImages.style.width = windowWidth * howManyChildren + "px";
     }
 
-    function slideAction () {
+    function slideAction() {
+        var button;
+
         for (i = 0; i < cantButtons; i++) {
-            var button = sliderButtons[i];
-            
-            button.addEventListener("click", function(e) {
-                var index = sliderButtons.indexOf(e.currentTarget);
+            button = sliderButtons[i];
+            button.addEventListener("click", slideAndClass.bind(null, i))
+        }        
+               
+    }
 
-                $sliderImages.style.left = windowWidth * (index) * (-1) + "px";
-                /*button.className = "sliderButtonActive";*/
-
-                /*if (button[i] === index) {
-                    button.className = "sliderButtonDeactive";
-                }*/
-            });
+    function slideAndClass(index) {
+        $sliderImages.style.left = windowWidth * index * (-1) + "px";
+        
+        for (i = 0; i < cantButtons; i++) {
+            sliderButtons[i].classList.remove('active');  
         }
+
+        sliderButtons[index].classList.toggle('active');
     }
 
     sliderWidth();
